53f29e7c5f711e9d44fbbc0506e0280b94b7aa01
Gleb Gulin <glepsgulin@gmail.com>
2021-08-03 23:34:19 +0300

    SVG icons

--- Spoodi/Spoodi/Resources/Images.cs
+++ Spoodi/Spoodi/Resources/Images.cs
@@ -6,10 +6,18 @@ namespace Spoodi.Resources
 {
     public static class Images
     {
-        const string SvgResourcesUrl = "resource://Unhar.Rider.Resources";
+        const string SvgResourcesUrl = "resource://Spoodi.Resources";
 
         //For other pictures
         public static string Burger = $"{SvgResourcesUrl}.burger.svg";
         public static string ArrowBack = $"{SvgResourcesUrl}.arrow_back.svg";
+        //For menu
+        public static string UserData = $"{SvgResourcesUrl}.personal_data.svg";
+        public static string Starred = $"{SvgResourcesUrl}.rate.svg";
+        public static string History = $"{SvgResourcesUrl}.history.svg";
+        public static string Card = $"{SvgResourcesUrl}.card.svg";
+        public static string MapGlobal = $"{SvgResourcesUrl}.global.svg";
+        public static string CustomerService = $"{SvgResourcesUrl}.customer_service.svg";
+        public static string Discount = $"{SvgResourcesUrl}.discount_svgrepo_com.svg";
     }
 }
